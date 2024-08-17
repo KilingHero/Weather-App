fa91178ded574b86a3915ba20b7a599f33743896
kilinghero <ib.safadi@seznam.cz>
2024-08-17 14:22:49 +0200

    vytvoření inputu a přidani seznam měst jako soubor json

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import PickCity from './components/PickCity'
 
 const App = () => {
   return (
-    <div>App</div>
+    <PickCity />
   )
 }
 
---
+++ src/components/PickCity.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const PickCity = () => {
+
+
+    return <>
+    <form action="">
+      <input
+        
+        type="text"
+        placeholder="Vyber město"
+      />
+      
+
+
+      
+   
+    </form>
+  </>;
+}
+
+export default PickCity

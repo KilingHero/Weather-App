69baabc9019ac476955d5174c4f5bd6e7d324881
kilinghero <ib.safadi@seznam.cz>
2024-08-17 16:05:53 +0200

    funkce pro získání dat o počasí

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import PickCity from './components/PickCity'
 
+
+
+//  funkce pro získání dat o počasí
+const getWeatherData = async (cityId) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=48af8bf98245f07f707f0489d11d219b&units=metric`
+  );
+  return response.json();
+};
 const App = () => {
   return (
     <PickCity />

4ed91e10bf688943960e11fd99c09409bb88244b
kilinghero <ib.safadi@seznam.cz>
2024-08-17 15:13:36 +0200

    Načtení seznamu měst z JSON souboru pomocí fetch

--- src/components/PickCity.jsx
+++ src/components/PickCity.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 
 const PickCity = () => {
 
+    const [cities, setCities] = useState([]);
+
+    useEffect(() => {
+        // Načtení seznamu měst z JSON souboru pomocí fetch
+        fetch("/city.list.json")
+          .then((response) => response.json())
+          .then((data) => {
+            setCities(data);
+          })
+          .catch((error) => {
+            console.error("Error loading city list:", error);
+          });
+      }, []);
 
     return <>
     <form action="">
